174180e8a005476650539e0060e26e4f2f979e82
Lêvy <levy.pereiraA1532@gmail.com>
2024-05-24 21:41:39 -0300

    Finish page from cart

--- public/views/cart.php
+++ public/views/cart.php
@@ -2,7 +2,7 @@
   <div class="WrapperCart__container">
     <div class="WrapperCart__container_products">
       <div class="CartTitle">
-        <h1>Todos os Produtos (1)</h1>
+        <h1>Todos os Produtos (<span></span>)</h1>
       </div>
 
       <div class="CartList">
@@ -37,11 +37,11 @@
     <div class="WrapperCart__container_info">
       <div class="CartInfo">
         <div class="CartInfo__price">
-          <p>Total Price: <span>R$ 111,60</span></p>
+          <p>Preço Total: <span>R$ 111,60</span></p>
         </div>
 
         <div class="CartInfo__button">
-          <button class="Button">Go to Checkout</button>
+          <button class="Button">Finalizar Compra</button>
         </div>
       </div>
     </div>
--- src/js/class/cart.js
+++ src/js/class/cart.js
@@ -61,24 +61,35 @@ class Cart {
   renderTotalPrice() {
     $('.HeaderCartPrice span').text(`R$ ${convertInBRL(this.total_value)}`);
     $('.button-cart-open span').text(this.cart.length);
+
+    $('.CartTitle span').text(this.cart.length);
+    $('.CartInfo__price span').text(`R$ ${convertInBRL(this.total_value)}`);
   }
 
   renderProject() {
     $('.HeaderCartList').html('');
+    $('.CartList').html('');
 
     if (this.cart.length > 0) {
       for (let x=0;x<this.cart.length;x++) {
         const project = this.cart[x];
   
-        const cartItemElement = this.cartItemProject(project);
-        $('.HeaderCartList')[0].appendChild(cartItemElement);
+        $('.HeaderCartList').each((i, h) => {
+          const cartItemElement = this.cartItemProjectHeader(project);
+          h.appendChild(cartItemElement);
+        });
+
+        const cartItemElement = this.cartItemProjectPage(project);
+        $('.CartList')[0].appendChild(cartItemElement);
       }
     } else {
-      $('.HeaderCartList').html('<p>Não há itens no carrinho!</p>');
+      const htmlNoneProducts = '<p>Não há itens no carrinho!</p>';
+      $('.HeaderCartList').html(htmlNoneProducts);
+      $('.CartList').html(htmlNoneProducts);
     }
   }
 
-  cartItemProject(project) {
+  cartItemProjectHeader(project) {
     const div = document.createElement('div');
     div.classList.add('CartItem');
 
@@ -108,5 +119,54 @@ class Cart {
     `;
 
     return div;
+  }
+
+  cartItemProjectPage(project) {
+    const div = document.createElement('div');
+    div.classList.add('CartItem');
+
+    let price = 0;
+    
+    if (!project.is_discount) {
+      price = project.price;
+    } else {
+      let discount = (project.price * project.discount_percent) / 100;
+      price = project.price - discount;
+    }
+
+    div.innerHTML = `
+      <div class="CartItem__image">
+        <img src="${project.cover}" alt="">
+      </div>
+
+      <div class="CartItem__info">
+        <div class="CartItem__info_title">
+          <p>${project.title}</p>
+        </div>
+
+        <div class="CartItem__info_text">
+          <p>${project.short_description}</p>
+        </div>
+
+        <div class="CartItem__info_price">
+          <p>R$ ${convertInBRL(price)}${project.is_discount ? ` <span>${project.discount_percent}% Desconto</span>` : ''}</p>
+        </div>
+      </div>
+
+      <div class="CartItem__actions"></div>
+    `;
+
+    $(div).find('.CartItem__actions')[0].appendChild(this.renderButtonCartPage());
+    $(div).find('.CartItem__actions button').on('click', () => this.removeProject(project));
+
+    return div;
+  }
+
+  renderButtonCartPage() {
+    const button = document.createElement('button');
+
+    button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>';
+
+    return button;
   }
 }
