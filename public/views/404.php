6de78d0b1876db67a469c6710ea4e3ebf5aabcae
Lêvy <levy.pereiraA1532@gmail.com>
2024-06-04 18:26:59 -0300

    Finish page not found

---
+++ public/views/404.php
@@ -0,0 +1,7 @@
+<main class="Wrapper404">
+  <div class="Wrapper404__container">
+    <h1>404</h1>
+    <p>Página não Encontrada</p>
+    <a href="<?= BASE ?>">Voltar para a Home</a>
+  </div>
+</main>
